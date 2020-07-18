049a1f2c7dfba9701eafffcc29a4ae813f0ed562
Julio César G. Costa <51680369+JCGCosta@users.noreply.github.com>
2020-07-18 20:59:14 -0300

    First Commit

---
+++ src/VCardConverter/VCardConverter/Form1.Designer.vb
@@ -0,0 +1,116 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnImport = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.dgvExibirXlsx = New System.Windows.Forms.DataGridView()
+        Me.btnExport = New System.Windows.Forms.Button()
+        Me.lblInfo = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.dgvExibirXlsx, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnImport
+        '
+        Me.btnImport.Enabled = False
+        Me.btnImport.Location = New System.Drawing.Point(12, 12)
+        Me.btnImport.Name = "btnImport"
+        Me.btnImport.Size = New System.Drawing.Size(126, 23)
+        Me.btnImport.TabIndex = 0
+        Me.btnImport.Text = "ImportarArquivoXls"
+        Me.btnImport.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblInfo)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 41)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(384, 42)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        '
+        'dgvExibirXlsx
+        '
+        Me.dgvExibirXlsx.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvExibirXlsx.Location = New System.Drawing.Point(12, 89)
+        Me.dgvExibirXlsx.Name = "dgvExibirXlsx"
+        Me.dgvExibirXlsx.Size = New System.Drawing.Size(384, 186)
+        Me.dgvExibirXlsx.TabIndex = 0
+        '
+        'btnExport
+        '
+        Me.btnExport.Enabled = False
+        Me.btnExport.Location = New System.Drawing.Point(258, 12)
+        Me.btnExport.Name = "btnExport"
+        Me.btnExport.Size = New System.Drawing.Size(138, 23)
+        Me.btnExport.TabIndex = 2
+        Me.btnExport.Text = "ExportarArquivoCsv"
+        Me.btnExport.UseVisualStyleBackColor = True
+        '
+        'lblInfo
+        '
+        Me.lblInfo.AutoSize = True
+        Me.lblInfo.Location = New System.Drawing.Point(6, 16)
+        Me.lblInfo.Name = "lblInfo"
+        Me.lblInfo.Size = New System.Drawing.Size(0, 13)
+        Me.lblInfo.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(294, 284)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(102, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Feito por Julio Cesar"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(407, 306)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExport)
+        Me.Controls.Add(Me.dgvExibirXlsx)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnImport)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Gerar VCards"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.dgvExibirXlsx, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnImport As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents dgvExibirXlsx As DataGridView
+    Friend WithEvents btnExport As Button
+    Friend WithEvents lblInfo As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ src/VCardConverter/VCardConverter/Form1.vb
@@ -0,0 +1,114 @@
+﻿Imports System.IO
+Imports System.Data
+
+Public Class Form1
+
+    Dim strFileName As String = ""
+
+    Private Function AccessDatabaseEngineInstalled() As Boolean
+
+        Dim FileName1 As String 'Location Prior to Windows 7
+        Dim FileName2 As String 'Location for Windows 7 (Not sure if this file location is for the 64bit version of Windows 7 only)
+
+        FileName1 = "C:\Program Files\Common Files\microsoft shared\OFFICE12\ACECORE.DLL"
+        FileName2 = "C:\Program Files (x86)\Common Files\microsoft shared\OFFICE12\ACECORE.DLL"
+
+        If File.Exists(FileName1) Or File.Exists(FileName2) Then
+            Return True
+        Else
+            Return False
+        End If
+
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        If AccessDatabaseEngineInstalled() = True Then
+            lblInfo.Text = "Programa Pronto para ser utilizado."
+            btnExport.Enabled = True
+            btnImport.Enabled = True
+        Else
+            lblInfo.Text = "Por Favor instale o conector Ole.Db."
+        End If
+
+    End Sub
+
+    Private Sub btnImport_Click(sender As Object, e As EventArgs) Handles btnImport.Click
+
+        lblInfo.Text = "Importando Arquivo."
+
+        Dim fd As OpenFileDialog = New OpenFileDialog()
+
+        fd.Title = "Abra um arquivo Xls"
+        fd.InitialDirectory = "C:\"
+        fd.Filter = "Excel Files(.xls)|*.xls"
+        fd.FilterIndex = 2
+        fd.RestoreDirectory = True
+
+        If fd.ShowDialog() = DialogResult.OK Then
+            strFileName = fd.FileName
+        Else
+            lblInfo.Text = "Programa Pronto para ser utilizado."
+        End If
+
+        Try
+
+            Dim MyConnection As System.Data.OleDb.OleDbConnection
+            Dim dataSet As System.Data.DataSet
+            Dim MyCommand As System.Data.OleDb.OleDbDataAdapter
+            Dim path As String = strFileName
+            Dim numLinhasdgv As Integer
+
+
+            If strFileName <> "" Then
+
+                MyConnection = New System.Data.OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties=Excel 12.0;")
+                MyCommand = New System.Data.OleDb.OleDbDataAdapter("select * from [VCardSheet$]", MyConnection)
+
+                dataSet = New System.Data.DataSet
+                MyCommand.Fill(dataSet)
+                dgvExibirXlsx.DataSource = dataSet.Tables(0)
+                numLinhasdgv = dgvExibirXlsx.Rows.Count - 1
+
+                MyConnection.Close()
+                lblInfo.Text = "Arquivo Importado, quantidade de linhas encontradas: " + numLinhasdgv.ToString
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Feche o programa Excel para continuar.", "Feche o excel.")
+        End Try
+
+
+    End Sub
+
+    Private Sub btnExport_Click(sender As Object, e As EventArgs) Handles btnExport.Click
+
+        lblInfo.Text = "Exportanto Arquivo csv."
+
+        'create empty string
+        Dim thecsvfile As String = String.Empty
+        'get the column headers
+        For Each column As DataGridViewColumn In dgvExibirXlsx.Columns
+            thecsvfile = thecsvfile & column.HeaderText & ","
+        Next
+        'trim the last comma
+        thecsvfile = thecsvfile.TrimEnd(",")
+        'Add the line to the output
+        thecsvfile = thecsvfile & vbCr & vbLf
+        'get the rows
+        For Each row As DataGridViewRow In dgvExibirXlsx.Rows
+            'get the cells
+            For Each cell As DataGridViewCell In row.Cells
+                thecsvfile = thecsvfile & cell.FormattedValue.replace(",", "") & ","
+            Next
+            'trim the last comma
+            thecsvfile = thecsvfile.TrimEnd(",")
+            'Add the line to the output
+            thecsvfile = thecsvfile & vbCr & vbLf
+        Next
+        'write the file
+        My.Computer.FileSystem.WriteAllText(strFileName.Replace("Importação.xls", "ImportaçãoGmail.csv"), thecsvfile, False)
+        MessageBox.Show("Arquivo csv pronto para ser utilizado." + vbCrLf + "Diretório: " + vbCrLf + strFileName.Replace("Importação.xls", "ImportaçãoGmail.csv"), "Arquivo Exportado!")
+    End Sub
+
+End Class
